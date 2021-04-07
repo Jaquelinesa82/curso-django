f07074c1bbbc4cbbff418db297ac30647069ae72
jaqueline <jaquelinesa.82@gmail.com>
2021-04-07 10:12:08 -0300

    Aplicação de entrega continua no CI
    close #29

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '5x2-(79dj2k=e6z*#b1y%jq00=w%8o)br!o$%2r3m#uwos-i-u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
