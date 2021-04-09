30213b5a77c17e2f9faf90fa2e3472dd9e5b0c9f
jaqueline <jaquelinesa.82@gmail.com>
2021-04-09 01:10:39 -0300

    Alterado a liguagem e fuso horário
    close #37

--- pypro/settings.py
+++ pypro/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#Configuração de ambiente de desenvolvimento
+# Configuração de ambiente de desenvolvimento
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
