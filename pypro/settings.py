deb1ce842fb7252b6f129db8d82e15a8424c8c74
jaqueline <jaquelinesa.82@gmail.com>
2021-04-10 16:39:46 -0300

    Customização de Usuário com Classe User e UserManager
    close #44

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())  # falta configurar o heroku com os dominios [precisa pagar heroku?]
-
+AUTH_USER_MODEL = 'base.User'
 
 # Application definition
 
