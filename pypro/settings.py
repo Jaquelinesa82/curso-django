d7488351e8e4cfba4e7759787fd693fb512f3451
jaqueline <jaquelinesa.82@gmail.com>
2021-04-09 22:12:46 -0300

    Configurando variável do AWS
    close #42

--- pypro/settings.py
+++ pypro/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collestfast',
     'django.contrib.staticfiles',
     'pypro.base'
 ]
