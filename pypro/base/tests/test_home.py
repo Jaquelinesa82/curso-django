bfc820e6e137f69e3af9748a6fe3c7c403a47115
jaqueline <jaquelinesa.82@gmail.com>
2021-04-16 21:35:27 -0300

    Encapsulando Urls em Apps
    close #70

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -6,8 +6,7 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
-    resp = client.get(reverse('base:home'))
-    return resp
+    return client.get(reverse('base:home'))
 
 
 def test_status_code(resp):
