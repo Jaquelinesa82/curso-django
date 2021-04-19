85184012c65fd2f5fdf2239434ce7ad27b3f65c1
jaqueline <jaquelinesa.82@gmail.com>
2021-04-19 17:01:51 -0300

    INcorṕrados primeiro video do Vimeo de forma manual.
    close #82

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -18,4 +18,4 @@ def test_titulo_video(resp):
 
 
 def test_conteudo_video(resp):
-    assert_contains(resp, f'<iframe width="560" height="315" src="https://www.youtube.com/embed/alALqQFykNs"')
+    assert_contains(resp, '<iframe width="560" height="315" src="https://www.youtube.com/embed/alALqQFykNs"')
