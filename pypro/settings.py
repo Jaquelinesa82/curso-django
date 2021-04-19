30b4c015489e42175136f30b2a42125a462e62f3
jaqueline <jaquelinesa.82@gmail.com>
2021-04-19 14:52:02 -0300

    Criado um app para conter as funcionalidades de videos
    close #80

--- pypro/settings.py
+++ pypro/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'collectfast',
     'django.contrib.staticfiles',
-    'pypro.base'
+    'pypro.base',
+    'pypro.aperitivos'
 ]
 
 MIDDLEWARE = [
