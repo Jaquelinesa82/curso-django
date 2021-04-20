ff8bd788eb65e8ce28b8da9afeb8bc8e367e0d7e
jaqueline <jaquelinesa.82@gmail.com>
2021-04-20 17:59:17 -0300

    Gerado páginas destintas com o slug
    close #88

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivos: Motivação', 'youtube_id': 'alALqQFykNs'}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivos: Motivação', 'youtube_id': 'alALqQFykNs'},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'youtube_id': 'ScmQ4I5Qr5s'}
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
