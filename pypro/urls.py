50d9e2ca60ba08cea900675a40aa49370dcfe4d3
jaqueline <jaquelinesa.82@gmail.com>
2021-04-20 00:15:33 -0300

    Definido conteúdos  blocos de templates .
    close #84

--- pypro/urls.py
+++ pypro/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
-    path('aperitivos', include('pypro.aperitivos.urls')),
+    path('aperitivos/', include('pypro.aperitivos.urls')),
 ]
 if settings.DEBUG:
     import debug_toolbar
