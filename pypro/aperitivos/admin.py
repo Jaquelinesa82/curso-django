5e30a2dac30d11b7eb5023f8d37b9f09c625f192
jaqueline <jaquelinesa.82@gmail.com>
2021-04-29 19:05:51 -0300

    Admin do Django close #114

--- pypro/aperitivos/admin.py
+++ pypro/aperitivos/admin.py
@@ -7,4 +7,4 @@ from pypro.aperitivos.models import Video
 class VideoAdmin(ModelAdmin):
     list_display = ('slug', 'titulo', 'creation', 'youtube_id')
     ordering = ('creation',)
-    prepopulated_fields = {'slug':('titulo',)}
+    prepopulated_fields = {'slug': ('titulo',)}
