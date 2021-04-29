8229c6db7a2104684cda4d7ab75eee63ffdf76ba
jaqueline <jaquelinesa.82@gmail.com>
2021-04-29 18:51:58 -0300

    Admin do Django close #114

--- pypro/aperitivos/admin.py
+++ pypro/aperitivos/admin.py
@@ -5,4 +5,6 @@ from pypro.aperitivos.models import Video
 
 @register(Video)
 class VideoAdmin(ModelAdmin):
-    pass
+    list_display = ('slug', 'titulo', 'creation', 'youtube_id')
+    ordering = ('creation',)
+    prepopulated_fields = {'slug':('titulo',)}
--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -10,3 +10,6 @@ class Video(models.Model):
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
+
+    def __str__(self):
+        return f'Video: {self.titulo}'
