dbd7dc6781ff7304c8157da8dbfa696e6d2d0b62
jaqueline <jaquelinesa.82@gmail.com>
2021-04-30 23:34:59 -0300

    app Django Ordered Model
    close #118

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from ordered_model.models import OrderedModel
 
 
-class Modulo(models.Model):
+class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
+    publico = models.TextField(blank=True)
+    descricao = models.TextField(blank=True)
+
+    class Meta (OrderedModel.Meta):
+        pass
 
     def __str__(self):
         return self.titulo
