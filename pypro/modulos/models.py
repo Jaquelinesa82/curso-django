7baedb30a56618eea1d723dc45e2930481e63611
jaqueline <jaquelinesa.82@gmail.com>
2021-05-07 17:23:37 -0300

    Gerado processo de migração de dados.
    close #120

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
 class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
-    publico = models.TextField(blank=True)
-    descricao = models.TextField(blank=True)
+    publico = models.TextField()
+    descricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
-    class Meta (OrderedModel.Meta):
+    class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
