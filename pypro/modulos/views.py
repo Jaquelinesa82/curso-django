05f6c90b914edfa3c6e85241d6ade274c34f8a95
jaqueline <jaquelinesa.82@gmail.com>
2021-05-07 18:49:14 -0300

    Gerado processo de migração de dados e implementação do Dropdown da aba de Módulos.
    close #120

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from urllib import request
+
+from django.shortcuts import render
+
+from pypro.modulos import facade
+
+
+def detalhe(requests, slug):
+    modulo = facade.encontrar_modulo(slug)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
