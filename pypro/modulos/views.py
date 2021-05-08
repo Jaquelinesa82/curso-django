217a33f9971b78bc0d5dae05ac67cbb7c7d21cd1
jaqueline <jaquelinesa.82@gmail.com>
2021-05-08 17:15:10 -0300

    Criado links das Aulas
    close #126

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -9,3 +9,7 @@ def detalhe(requests, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_modulo_ordenados(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    pass
