4fd5284276d7e3575f6ba01ba2c9ebfef1978e2f
jaqueline <jaquelinesa.82@gmail.com>
2021-05-13 01:07:21 -0300

    Admin com Modelos Relacionados
    close #142

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from pypro.turmas.models import Turma
 
 
+class MatriculaInline(admin.TabularInline):
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario', )
+    ordering = ('-data', )
+
+
 # Register your models here.
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInline]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
