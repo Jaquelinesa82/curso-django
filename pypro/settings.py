bea6f31e5a91db89c1907aa2d5b533a2e8b16541
jaqueline <jaquelinesa.82@gmail.com>
2021-05-17 21:07:02 -0300

    Configuração de variáveis de ambiente Sendgrid no Heroku
    close #156

--- pypro/settings.py
+++ pypro/settings.py
@@ -6,8 +6,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
 from functools import partial
 
 import dj_database_url
